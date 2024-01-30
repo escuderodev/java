5756c48ddda57d481859a7e503b206a941aab838
escuderodev <escudero.ti@gmail.com>
2024-01-29 23:52:38 -0300

    feat: paramos na aula: https://cursos.alura.com.br/course/java-trabalhando-lambdas-streams-spring-framework/task/136748

--- screenmatch/src/main/java/br/com/escuderodev/screenmatch/service/IConverteDados.java
+++ screenmatch/src/main/java/br/com/escuderodev/screenmatch/service/IConverteDados.java
@@ -1,7 +1,5 @@
 package br.com.escuderodev.screenmatch.service;
 
-import br.com.escuderodev.screenmatch.model.DadosSerie;
-
 public interface IConverteDados {
     <T> T obterDados(String json, Class<T> classe);
 }
